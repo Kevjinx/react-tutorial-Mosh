2a7bf3ba6b5a1ced6169c5a1b742548447a986bb
kevjinx <kevin415mei@gmail.com>
2023-01-20 11:02:25 -0800

    add getBadgeClasses

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -3,16 +3,30 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
 	state = {
-		count: 0
+		count: 0,
 	}
 
 	render() {
+
 		return (
 			<div>
-				<span>{this.state.count}</span>
-				<button>Increment</button>
+				<span className={this.getBadgeClasses()} >
+					{this.formatCount()}
+				</span>
+				<button className="btn btn-secondary btn-sm">Increment</button>
 			</div>
 		);
+	}
+
+	getBadgeClasses() {
+		let classes = "badge m-2 ";
+		classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+		return classes;
+	}
+
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? 'Zero' : count;
 	}
 
 
